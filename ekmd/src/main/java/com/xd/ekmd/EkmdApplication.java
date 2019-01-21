73077ae03f8bcadd4928ec12c762872f81a1b56b
jinzx <sx-jinzx@dtdream.com>
2019-01-21 19:52:22 +0800

    改造博客

---
+++ ekmd/src/main/java/com/xd/ekmd/EkmdApplication.java
@@ -0,0 +1,14 @@
+package com.xd.ekmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkmdApplication.class, args);
+	}
+
+}
+
---
+++ ekmd/src/main/java/com/xd/ekmd/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.xd.ekmd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
